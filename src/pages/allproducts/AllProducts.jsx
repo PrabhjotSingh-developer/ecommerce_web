552009985144fbe3d5d7e4a737d5df8ddb53d69c
Prabhjot Singh <prabhjotsingh1032003@gmail.com>
2024-02-18 19:04:05 +0530

    filter added

--- src/pages/allproducts/AllProducts.jsx
+++ src/pages/allproducts/AllProducts.jsx
@@ -6,37 +6,58 @@ import Layout from "../../components/layout/Layout";
 import { Link } from "react-router-dom";
 const AllProducts = () => {
   const {loading} = useContext(MyContext)
-  const [selectedValue,setSelectedValue] = useState('All')
+ 
   const [showPro,setShowPro] = useState([])
+  const [viewData,setViewData] = useState([])
+  const [selectedValue,setSelectedValue] = useState("")
   function filterProduct(e)
   {
     let data = JSON.parse(localStorage.getItem('AllProductsData'))
-    // console.log(data)
     setShowPro(data)
     let arr = []
     let f = e.target.value
-    console.log(f)
-    setSelectedValue(f)
-    console.log(selectedValue)
-    // console.log(f)
-      if(selectedValue === "All")
-          return
+      if(f === "All")
+        return
        
-         arr = showPro.filter((item)=>item.category === selectedValue)
-         console.log(arr)
-         setShowPro(arr)
+         arr = viewData.filter((item)=>item.category === f)
         
-       
-       
+         setShowPro(arr)
+      setSelectedValue("")
+     
+  }
+  function filterPrice(e)
+  {
+    let arr = []
+     
+
+    let f = e.target.value
+    console.log(f)
+    if(f === "low")
+    {
+       arr =  showPro.sort((a,b)=>a.price - b.price)
+       setSelectedValue("low")
+    }
+    else if(f==="high"){
+      arr =  showPro.sort((a,b)=>b.price - a.price)
+      setSelectedValue("high")
       
+      
+    }
+  
+    setShowPro([])
+    setShowPro([...showPro,arr]) 
+    
+    // console.log(showPro)
+    // setShowPro(arr)
   }
   useEffect(()=>{
       let data = JSON.parse(localStorage.getItem('AllProductsData'))
       setShowPro(data)
+      setViewData(data)
   },[])
   return (
     <Layout>
-      <div className="flex ">
+      <div className="flex flex-col">
          <div className="innerfilter flex justify-center items-center w-[100%] ">
             <label htmlFor="Category" className="mx-4">Category</label>
             <select name="Category" defaultChecked="All"  className="mr-5" id="" onChange={filterProduct}>
@@ -47,23 +68,28 @@ const AllProducts = () => {
              </select>
             <label htmlFor="Price"  className="mx-4">Price</label>
 
-             <select name="Price" className="mr-5" id="" >
-                <option value="">Low to High</option>
-                <option value=""> High to Low</option>
+             <select name="Price" className="mr-5" id="" value={selectedValue} onChange={filterPrice}>
+                <option value="">Select by Price</option>
+                <option value="low">Low to High</option>
+                <option value="high"> High to Low</option>
              </select>
          </div>
-          <div className="allProducts">
+          <div className="allProducts grid gap-10 lg:grid-cols-3 md:grid-cols-2 place-content-center mt-20 w-[80%] mx-auto ">
                {
                   loading ? <Loader/>:
                   (
-                     showPro.map((item,index)=>{
-                       return  <div
-                       className="productItem w-[100%] p-5 border rounded-lg relative "
+                     
+                     showPro?.map((item,index)=>{
+                      if(item.title!=undefined)
+                      {
+                         
+                       return   <div
+                       className="productItem w-[90%] p-5 border rounded-lg gap-5 flex flex-col mx-auto"
                        key={index}
                      >
                        <Link to={`/productinfo/:${item.id}`}>
-                         <div className="product_img w-[100%] p-2 h-[300px] hover:scale-105 transition-all">
-                           <img src={item.imageUrl} className=" " alt="" />
+                         <div className="product_img w-[100%] p-2  ">
+                           <img src={item?.imageUrl} className=" " alt="" />
                          </div>
                        </Link>
                        <div
@@ -73,7 +99,7 @@ const AllProducts = () => {
                            <p>E-bharat</p>
                            <h2 className="font-semibold text-xl">
                              {" "}
-                             {item.title.substring(0, 10)}
+                             {item?.title?.substring(0, 10)}
                            </h2>
                          </Link>
                          <h4 className="font-semibold">₹{item.price}</h4>
@@ -85,6 +111,7 @@ const AllProducts = () => {
                          </button>
                        </div>
                      </div>
+                      }
                      })
                    
                     
